0840ce438f0b5489966144e13b5253ecbae41f96
Matthew <matthew@Velmas-MacBook-Air.local>
2020-03-18 18:21:15 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+app.use('/static', express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs')
+console.log("_dirname")
+console.log(__dirname)
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
