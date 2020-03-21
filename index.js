859e28c8b9fe41916a47b1ac7418dbe08f56ff95
Matthew <matthew@Velmas-MacBook-Air.local>
2020-03-21 06:40:43 -0500

    added a timeline route

--- index.js
+++ index.js
@@ -8,5 +8,7 @@ console.log("_dirname")
 console.log(__dirname)
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'))
+app.get('/timeline', (req, res) => res.sendFile(__dirname + '/public/timeline.html'))
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
