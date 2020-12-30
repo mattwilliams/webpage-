58f9ff986d35e93c7d5dedb1095e8f47a5d54e1c
Matthew Williams <matthew@venus.local>
2020-12-30 12:03:58 -0600

    editing a commit yay added eco config

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps : [{
+    script: 'index.js',
+    watch: '.'
+  }, {
+    script: './service-worker/',
+    watch: ['./service-worker']
+  }],
+
+  deploy : {
+    production : {
+      user : 'matthew',
+      host : '192.168.1.5',
+      ref  : 'origin/master',
+      repo : 'git@github.com:mattwilliams/webpage-.git',
+      path : '~/webpage-/',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
